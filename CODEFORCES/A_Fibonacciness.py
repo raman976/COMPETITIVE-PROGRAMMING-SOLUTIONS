77e4cde03229960cbf537166ca875be210359df4
Raman Pandey <ramanpandey413@gmail.com>
2025-01-19 23:36:42 +0530

    UPLOADING LEETCODE DAY 4 19 JAN 2025

--- CODEFORCES/A_Fibonacciness.py
+++ CODEFORCES/A_Fibonacciness.py
@@ -1,9 +1,7 @@
 """
 A. Fibonacciness
-time limit per test
-1 second
-memory limit per test
-256 megabytes
+time limit per test 1 second
+memory limit per test 256 megabytes
 
 There is an array of 5
 integers. Initially, you only know a1,a2,a4,a5. You may set a3 to any positive integer, negative integer, or zero. The Fibonacciness of the array is the number of integers i (1≤i≤3) such that ai+2=ai+ai+1. Find the maximum Fibonacciness over all integer values of a3
@@ -12,14 +10,10 @@ integers. Initially, you only know a1,a2,a4,a5. You may set a3 to any positive i
 Input
 
 The first line contains an integer t
-(1≤t≤500
-
-) — the number of test cases.
+(1≤t≤500) — the number of test cases.
 
 The only line of each test case contains four integers a1,a2,a4,a5
-(1≤ai≤100
-
-).
+(1≤ai≤100).
 Output
 
 For each test case, output the maximum Fibonacciness on a new line.
