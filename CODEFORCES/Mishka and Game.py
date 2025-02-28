8e45ad2778409c38133f32c4ed860e54434bc597
Raman Pandey <ramanpandey413@gmail.com>
2025-02-28 19:37:45 +0530

    SOLVING CODEFORCES DAY 41 28 FEB 2025

---
+++ CODEFORCES/Mishka and Game.py
@@ -0,0 +1,15 @@
+n=int(input())
+m=0
+c=0
+for i in range(n):
+    a,b=map(int,input().split())
+    if a>b:
+        m+=1
+    if a<b:
+        c+=1
+if m>c:
+    print("Mishka")
+elif m<c:
+    print("Chris")
+else:
+    print("Friendship is magic!^^")
