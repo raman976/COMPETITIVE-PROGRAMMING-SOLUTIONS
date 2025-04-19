f437efe1786ca6a12ffb7b5af48d8c1b02f88330
Raman Pandey <ramanpandey413@gmail.com>
2025-04-19 23:44:14 +0530

    SOLVING LEETCODE DAY 90 19 APR 2025

---
+++ LEETCODE/Valid Parentheses.py
@@ -0,0 +1,68 @@
+#################################################################################
+#################################################################################
+'''
+20. Valid Parentheses
+Solved
+Easy
+Topics
+Companies
+Hint
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.
+
+
+'''
+#################################################################################
+#################################################################################
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in bracket_map.values():  
+                stack.append(char)
+            elif char in bracket_map: 
+                if not stack or stack[-1] != bracket_map[char]:
+                    return False
+                stack.pop()
+        return not stack
