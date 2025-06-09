379f83efad49e25dc8cdce3446d9b822d17e4eaa
Raman Pandey <ramanpandey413@gmail.com>
2025-06-09 22:33:40 +0530

    SOLVING CODEFORCES DAY 141 9 JUN 2025

---
+++ CODEFORCES/Say Hello With C++.cpp
@@ -0,0 +1,35 @@
+// A. Say Hello With C++
+// time limit per test
+// 1 second
+// memory limit per test
+// 256 megabytes
+
+// Given a name S. Print "Hello, (name)" without parentheses.
+// Input
+
+// Only one line containing a string S.
+// Output
+
+// Print "Hello, " without quotes, then print name.
+// Example
+// Input
+// Copy
+
+// programmer
+
+// Output
+// Copy
+
+// Hello, programmer
+
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <set>
+#include <map>
+
+int main() {
+    string a;
+    cin>>a;
+    cout<<"Hello"<<", "<<a;
+}
