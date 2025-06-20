d8824d2049af5dc6e1b0c04060037c473481c982
Raman Pandey <ramanpandey413@gmail.com>
2025-06-20 22:40:35 +0530

    SOLVING CODEFORCES DAY 152 20 JUN 2025

---
+++ CODEFORCES/Devu and his Brother.cpp
@@ -0,0 +1,182 @@
+//        ／＞　 フ
+//        | 　_　_|    ← This lil guy runs it fine
+//      ／` ミ＿xノ
+//     /　　　　 |
+//    /　 ヽ　　 ﾉ
+//    │　　|　|　|
+//  ／￣|　　 |　|　|
+// (￣ヽ＿_ヽ_)__)
+//  ＼二)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// DEBUGGING TEMPLATE
+#ifndef DEBUG_TEMPLATE_CPP
+#define DEBUG_TEMPLATE_CPP
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace __DEBUG_UTIL__
+{
+    template <typename T>
+    concept is_iterable = requires(T &&x) { begin(x); } &&
+                          !is_same_v<remove_cvref_t<T>, string>;
+
+    void print(const char *x) { cerr << x; }
+    void print(char x) { cerr << "'" << x << "'"; }
+    void print(bool x) { cerr << (x ? "T" : "F"); }
+    void print(string x) { cerr << '"' << x << '"'; }
+
+    void print(vector<bool> &v) {
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : v)
+            cerr << (f++ ? "," : "") << (i ? "T" : "F");
+        cerr << "}";
+    }
+
+    template <typename T>
+    void print(T &&x) {
+        if constexpr (is_iterable<T>) {
+            if (size(x) && is_iterable<decltype(*(begin(x)))>) {
+                int f = 0;
+                cerr << "\n~~~~~\n";
+                int w = max(0LL, (long long)log10(size(x) - 1)) + 2;
+                for (auto &&i : x) {
+                    cerr << setw(w) << left << f++, print(i), cerr << "\n";
+                }
+                cerr << "~~~~~\n";
+            } else {
+                int f = 0;
+                cerr << "{";
+                for (auto &&i : x)
+                    cerr << (f++ ? "," : ""), print(i);
+                cerr << "}";
+            }
+        } else if constexpr (requires { x.pop(); }) {
+            auto temp = x;
+            int f = 0;
+            cerr << "{";
+            if constexpr (requires { x.top(); })
+                while (!temp.empty())
+                    cerr << (f++ ? "," : ""), print(temp.top()), temp.pop();
+            else
+                while (!temp.empty())
+                    cerr << (f++ ? "," : ""), print(temp.front()), temp.pop();
+            cerr << "}";
+        } else if constexpr (requires { x.first; x.second; }) {
+            cerr << '(', print(x.first), cerr << ',', print(x.second), cerr << ')';
+        } else if constexpr (requires { get<0>(x); }) {
+            int f = 0;
+            cerr << '(', apply([&f](auto... args) { ((cerr << (f++ ? "," : ""), print(args)), ...); }, x);
+            cerr << ')';
+        } else
+            cerr << x;
+    }
+
+    template <typename T, typename... V>
+    void printer(const char *names, T &&head, V &&...tail) {
+        int i = 0;
+        for (int bracket = 0; names[i] != '\0' && (names[i] != ',' || bracket > 0); i++)
+            if (names[i] == '(' || names[i] == '<' || names[i] == '{')
+                bracket++;
+            else if (names[i] == ')' || names[i] == '>' || names[i] == '}')
+                bracket--;
+        cerr.write(names, i) << " = ";
+        print(head);
+        if constexpr (sizeof...(tail))
+            cerr << " ||", printer(names + i + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+
+    template <typename T, typename... V>
+    void printerArr(const char *names, T arr[], size_t N, V... tail) {
+        size_t i = 0;
+        for (; names[i] && names[i] != ','; i++)
+            cerr << names[i];
+        for (i++; names[i] && names[i] != ','; i++);
+        cerr << " = {";
+        for (size_t ind = 0; ind < N; ind++)
+            cerr << (ind ? "," : ""), print(arr[ind]);
+        cerr << "}";
+        if constexpr (sizeof...(tail))
+            cerr << " ||", printerArr(names + i + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#define debugArr(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+#endif
+int m,n;
+vi a;
+vi b;
+int helper(int k){
+    int ans=0;
+    for(int i=0;i<n;i++){
+        if(a[i]<k){
+            ans+=k-a[i];
+        }
+    }
+    for(int i=0;i<m;i++){
+        if(b[i]>k){
+            ans+=b[i]-k;
+        }
+    }
+    return ans;
+}
+void solve() {
+    
+    cin>>n>>m;
+    a.resize(n);
+    b.resize(m);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < m; ++i) cin >> b[i];
+    int low=0;
+    int high=1e9;
+    int answer=1e18;
+    while(high>=low+2){
+        int mid1=low+(high-low)/3;
+        int mid2=high-(high-low)/3;
+        int ops1=helper(mid1);
+        int ops2=helper(mid2);
+        if(ops1<=ops2){
+            high=mid2-1;
+        }else{
+            low=mid1+1;
+        }
+    }
+   for (int i = low; i <= high; i++) {
+        answer = min(answer, helper(i));
+    }
+
+    cout << answer << '\n';
+}
+
+#undef int
+int main(int argc, char const *argv[]) {
+    #define int long long
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
