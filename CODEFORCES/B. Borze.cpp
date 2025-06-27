2667ea896e72fde653aa24fd1ab5a612614e848d
Raman Pandey <ramanpandey413@gmail.com>
2025-06-27 20:39:39 +0530

    SOLVING CODEFORCES DAY 159 27 JUN 2025

---
+++ CODEFORCES/B. Borze.cpp
@@ -0,0 +1,228 @@
+// B. Borze
+// time limit per test
+// 2 seconds
+// memory limit per test
+// 256 megabytes
+
+// Ternary numeric notation is quite popular in Berland. To telegraph the ternary number the Borze alphabet is used. Digit 0 is transmitted as «.», 1 as «-.» and 2 as «--». You are to decode the Borze code, i.e. to find out the ternary number given its representation in Borze alphabet.
+// Input
+
+// The first line contains a number in Borze code. The length of the string is between 1 and 200 characters. It's guaranteed that the given string is a valid Borze code of some ternary number (this number can have leading zeroes).
+// Output
+
+// Output the decoded ternary number. It can have leading zeroes.
+// Examples
+// Input
+// Copy
+
+// .-.--
+
+// Output
+// Copy
+
+// 012
+
+// Input
+// Copy
+
+// --.
+
+// Output
+// Copy
+
+// 20
+
+// Input
+// Copy
+
+// -..-.--
+
+// Output
+// Copy
+
+// 1012
+
+//        ／＞　 フ
+//        | 　_　_|    ← This lil guy runs it fine
+//      ／` ミ＿xノ
+//     /　　　　 |
+//    /　 ヽ　　 ﾉ
+//    │　　|　|　|
+//  ／￣|　　 |　|　|
+// (￣ヽ＿_ヽ_)__)
+//  ＼二)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// DEBUGGING TEMPLATE
+#ifndef DEBUG_TEMPLATE_CPP
+#define DEBUG_TEMPLATE_CPP
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace __DEBUG_UTIL__
+{
+    template <typename T>
+    concept is_iterable = requires(T &&x) { begin(x); } &&
+                          !is_same_v<remove_cvref_t<T>, string>;
+
+    void print(const char *x) { cerr << x; }
+    void print(char x) { cerr << "'" << x << "'"; }
+    void print(bool x) { cerr << (x ? "T" : "F"); }
+    void print(string x) { cerr << '"' << x << '"'; }
+
+    void print(vector<bool> &v)
+    {
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : v)
+            cerr << (f++ ? "," : "") << (i ? "T" : "F");
+        cerr << "}";
+    }
+
+    template <typename T>
+    void print(T &&x)
+    {
+        if constexpr (is_iterable<T>)
+        {
+            if (size(x) && is_iterable<decltype(*(begin(x)))>)
+            {
+                int f = 0;
+                cerr << "\n~~~~~\n";
+                int w = max(0LL, (long long)log10(size(x) - 1)) + 2;
+                for (auto &&i : x)
+                {
+                    cerr << setw(w) << left << f++, print(i), cerr << "\n";
+                }
+                cerr << "~~~~~\n";
+            }
+            else
+            {
+                int f = 0;
+                cerr << "{";
+                for (auto &&i : x)
+                    cerr << (f++ ? "," : ""), print(i);
+                cerr << "}";
+            }
+        }
+        else if constexpr (requires { x.pop(); })
+        {
+            auto temp = x;
+            int f = 0;
+            cerr << "{";
+            if constexpr (requires { x.top(); })
+                while (!temp.empty())
+                    cerr << (f++ ? "," : ""), print(temp.top()), temp.pop();
+            else
+                while (!temp.empty())
+                    cerr << (f++ ? "," : ""), print(temp.front()), temp.pop();
+            cerr << "}";
+        }
+        else if constexpr (requires { x.first; x.second; })
+        {
+            cerr << '(', print(x.first), cerr << ',', print(x.second), cerr << ')';
+        }
+        else if constexpr (requires { get<0>(x); })
+        {
+            int f = 0;
+            cerr << '(', apply([&f](auto... args)
+                               { ((cerr << (f++ ? "," : ""), print(args)), ...); }, x);
+            cerr << ')';
+        }
+        else
+            cerr << x;
+    }
+
+    template <typename T, typename... V>
+    void printer(const char *names, T &&head, V &&...tail)
+    {
+        int i = 0;
+        for (int bracket = 0; names[i] != '\0' && (names[i] != ',' || bracket > 0); i++)
+            if (names[i] == '(' || names[i] == '<' || names[i] == '{')
+                bracket++;
+            else if (names[i] == ')' || names[i] == '>' || names[i] == '}')
+                bracket--;
+        cerr.write(names, i) << " = ";
+        print(head);
+        if constexpr (sizeof...(tail))
+            cerr << " ||", printer(names + i + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+
+    template <typename T, typename... V>
+    void printerArr(const char *names, T arr[], size_t N, V... tail)
+    {
+        size_t i = 0;
+        for (; names[i] && names[i] != ','; i++)
+            cerr << names[i];
+        for (i++; names[i] && names[i] != ','; i++)
+            ;
+        cerr << " = {";
+        for (size_t ind = 0; ind < N; ind++)
+            cerr << (ind ? "," : ""), print(arr[ind]);
+        cerr << "}";
+        if constexpr (sizeof...(tail))
+            cerr << " ||", printerArr(names + i + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#define debugArr(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+#endif
+
+void solve()
+{
+    string s;
+    cin >> s;
+    string result;
+    for (int i = 0; i < s.size();)
+    {
+        if (s[i] == '.')
+        {
+            result += '0';
+            i++;
+        }
+        else if (s[i] == '-' && s[i + 1] == '.')
+        {
+            result += '1';
+            i += 2;
+        }
+        else if (s[i] == '-' && s[i + 1] == '-')
+        {
+            result += '2';
+            i += 2;
+        }
+    }
+    cout << result << endl;
+}
+
+#undef int
+int main(int argc, char const *argv[])
+{
+#define int long long
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
