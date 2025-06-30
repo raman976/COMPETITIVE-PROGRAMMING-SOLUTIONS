4c10407f9d3cd45a917fd73315026eadb71b97aa
Raman Pandey <ramanpandey413@gmail.com>
2025-06-30 20:28:39 +0530

    SOLVING CODEFORCES DAY 162 30 JUN 2025

---
+++ CODEFORCES/A. Colorful Stones (Simplified Edition).cpp
@@ -0,0 +1,205 @@
+// A. Colorful Stones (Simplified Edition)
+// time limit per test
+// 2 seconds
+// memory limit per test
+// 256 megabytes
+
+// There is a sequence of colorful stones. The color of each stone is one of red, green, or blue. You are given a string s. The i-th (1-based) character of s represents the color of the i-th stone. If the character is "R", "G", or "B", the color of the corresponding stone is red, green, or blue, respectively.
+
+// Initially Squirrel Liss is standing on the first stone. You perform instructions one or more times.
+
+// Each instruction is one of the three types: "RED", "GREEN", or "BLUE". After an instruction c, if Liss is standing on a stone whose colors is c, Liss will move one stone forward, else she will not move.
+
+// You are given a string t. The number of instructions is equal to the length of t, and the i-th character of t represents the i-th instruction.
+
+// Calculate the final position of Liss (the number of the stone she is going to stand on in the end) after performing all the instructions, and print its 1-based position. It is guaranteed that Liss don't move out of the sequence.
+// Input
+
+// The input contains two lines. The first line contains the string s (1 ≤ |s| ≤ 50). The second line contains the string t (1 ≤ |t| ≤ 50). The characters of each string will be one of "R", "G", or "B". It is guaranteed that Liss don't move out of the sequence.
+// Output
+
+// Print the final 1-based position of Liss in a single line.
+// Examples
+// Input
+// Copy
+
+// RGB
+// RRR
+
+// Output
+// Copy
+
+// 2
+
+// Input
+// Copy
+
+// RRRBGBRBBB
+// BBBRR
+
+// Output
+// Copy
+
+// 3
+
+// Input
+// Copy
+
+// BRRBGBRGRBGRGRRGGBGBGBRGBRGRGGGRBRRRBRBBBGRRRGGBBB
+// BBRBGGRGRGBBBRBGRBRBBBBRBRRRBGBBGBBRRBBGGRBRRBRGRB
+
+// Output
+// Copy
+
+// 15
+
+//        ／＞　 フ
+//        | 　_　_|    ← This lil guy runs it fine
+//      ／` ミ＿xノ
+//     /　　　　 |
+//    /　 ヽ　　 ﾉ
+//    │　　|　|　|
+//  ／￣|　　 |　|　|
+// (￣ヽ＿_ヽ_)__)
+//  ＼二)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// DEBUGGING TEMPLATE
+#ifndef DEBUG_TEMPLATE_CPP
+#define DEBUG_TEMPLATE_CPP
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace __DEBUG_UTIL__
+{
+    template <typename T>
+    concept is_iterable = requires(T &&x) { begin(x); } &&
+                          !is_same_v<remove_cvref_t<T>, string>;
+
+    void print(const char *x) { cerr << x; }
+    void print(char x) { cerr << "'" << x << "'"; }
+    void print(bool x) { cerr << (x ? "T" : "F"); }
+    void print(string x) { cerr << '"' << x << '"'; }
+
+    void print(vector<bool> &v) {
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : v)
+            cerr << (f++ ? "," : "") << (i ? "T" : "F");
+        cerr << "}";
+    }
+
+    template <typename T>
+    void print(T &&x) {
+        if constexpr (is_iterable<T>) {
+            if (size(x) && is_iterable<decltype(*(begin(x)))>) {
+                int f = 0;
+                cerr << "\n~~~~~\n";
+                int w = max(0LL, (long long)log10(size(x) - 1)) + 2;
+                for (auto &&i : x) {
+                    cerr << setw(w) << left << f++, print(i), cerr << "\n";
+                }
+                cerr << "~~~~~\n";
+            } else {
+                int f = 0;
+                cerr << "{";
+                for (auto &&i : x)
+                    cerr << (f++ ? "," : ""), print(i);
+                cerr << "}";
+            }
+        } else if constexpr (requires { x.pop(); }) {
+            auto temp = x;
+            int f = 0;
+            cerr << "{";
+            if constexpr (requires { x.top(); })
+                while (!temp.empty())
+                    cerr << (f++ ? "," : ""), print(temp.top()), temp.pop();
+            else
+                while (!temp.empty())
+                    cerr << (f++ ? "," : ""), print(temp.front()), temp.pop();
+            cerr << "}";
+        } else if constexpr (requires { x.first; x.second; }) {
+            cerr << '(', print(x.first), cerr << ',', print(x.second), cerr << ')';
+        } else if constexpr (requires { get<0>(x); }) {
+            int f = 0;
+            cerr << '(', apply([&f](auto... args) { ((cerr << (f++ ? "," : ""), print(args)), ...); }, x);
+            cerr << ')';
+        } else
+            cerr << x;
+    }
+
+    template <typename T, typename... V>
+    void printer(const char *names, T &&head, V &&...tail) {
+        int i = 0;
+        for (int bracket = 0; names[i] != '\0' && (names[i] != ',' || bracket > 0); i++)
+            if (names[i] == '(' || names[i] == '<' || names[i] == '{')
+                bracket++;
+            else if (names[i] == ')' || names[i] == '>' || names[i] == '}')
+                bracket--;
+        cerr.write(names, i) << " = ";
+        print(head);
+        if constexpr (sizeof...(tail))
+            cerr << " ||", printer(names + i + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+
+    template <typename T, typename... V>
+    void printerArr(const char *names, T arr[], size_t N, V... tail) {
+        size_t i = 0;
+        for (; names[i] && names[i] != ','; i++)
+            cerr << names[i];
+        for (i++; names[i] && names[i] != ','; i++);
+        cerr << " = {";
+        for (size_t ind = 0; ind < N; ind++)
+            cerr << (ind ? "," : ""), print(arr[ind]);
+        cerr << "}";
+        if constexpr (sizeof...(tail))
+            cerr << " ||", printerArr(names + i + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#define debugArr(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+#endif
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+    int pos = 0;
+    for (char c : t) {
+        if (s[pos] == c) {
+            pos++;
+        }
+    }
+    cout << pos + 1 << endl;
+}
+
+#undef int
+int main(int argc, char const *argv[]) {
+    #define int long long
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
