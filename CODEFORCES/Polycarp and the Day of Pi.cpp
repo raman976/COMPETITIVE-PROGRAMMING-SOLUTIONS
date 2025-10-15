9086c50e5d7eb7b6a2b5a4d45f7a35133f59f6c1
Raman Pandey <ramanpandey413@gmail.com>
2025-10-15 23:44:44 +0530

    SOLVING CODEFORCES DAY 268 15 OCT 2025

---
+++ CODEFORCES/Polycarp and the Day of Pi.cpp
@@ -0,0 +1,85 @@
+// A. Polycarp and the Day of Pi
+// time limit per test
+// 1 second
+// memory limit per test
+// 256 megabytes
+
+// On March 14, the day of the number π
+
+// is celebrated all over the world. This is a very important mathematical constant equal to the ratio of the circumference of a circle to its diameter.
+
+// Polycarp was told at school that the number π
+// is irrational, therefore it has an infinite number of digits in decimal notation. He wanted to prepare for the Day of the number π
+
+// by memorizing this number as accurately as possible.
+
+// Polycarp wrote out all the digits that he managed to remember. For example, if Polycarp remembered π
+// as 3.1415
+
+// , he wrote out 31415.
+
+// Polycarp was in a hurry and could have made a mistake, so you decided to check how many first digits of the number π
+
+// Polycarp actually remembers correctly.
+// Input
+
+// The first line of the input data contains the single integer t
+// (1≤t≤103
+
+// ) — the number of test cases in the test.
+
+// Each test case is described by a single string of digits n
+
+// , which was written out by Polycarp.
+
+// The string n
+// contains up to 30
+
+// digits.
+// Output
+
+// Output t
+// integers, each of which is the answer to the corresponding test case, that is how many first digits of the number π
+
+// Polycarp remembers correctly.
+// Example
+// Input
+// Copy
+
+// 9
+// 000
+// 3
+// 4141592653
+// 141592653589793238462643383279
+// 31420
+// 31415
+// 314159265358
+// 27182
+// 314159265358979323846264338327
+
+// Output
+// Copy
+
+// 0
+// 1
+// 0
+// 0
+// 3
+// 5
+// 12
+// 0
+// 30
+
+
+
+
+
+
+t = int(input())
+pi = '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
+for _ in range(t):
+    n = input() + '#'
+    for i in range(len(n)):
+        if pi[i] != n[i]:
+            print(i)
+            break
