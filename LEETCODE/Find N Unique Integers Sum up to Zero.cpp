fa5cccdd4181f7231aba5f956e93368cd9b30b70
Raman Pandey <ramanpandey413@gmail.com>
2025-12-14 23:55:37 +0530

    SOLVING LEETCODE DAY 328 14 DEC 2025

---
+++ LEETCODE/Find N Unique Integers Sum up to Zero.cpp
@@ -0,0 +1,56 @@
+// 1304. Find N Unique Integers Sum up to Zero
+// Solved
+// Easy
+// Topics
+// premium lock iconCompanies
+// Hint
+
+// Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+ 
+
+// Example 1:
+
+// Input: n = 5
+// Output: [-7,-1,1,3,4]
+// Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+// Example 2:
+
+// Input: n = 3
+// Output: [-1,0,1]
+
+// Example 3:
+
+// Input: n = 1
+// Output: [0]
+
+ 
+
+// Constraints:
+
+//     1 <= n <= 1000
+
+ 
+
+
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> result;
+        
+
+        if (n % 2 == 1) {
+            result.push_back(0);
+            n--;
+        }
+        
+
+        for (int i = 1; i <= n / 2; i++) {
+            result.push_back(i);
+            result.push_back(-i);
+        }
+        
+        return result;
+    }
+};
